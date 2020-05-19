f0fd8315c5fa6c22275ca89d62a361d2ad2a142e
Alwin Esch <alwin.esch@web.de>
2020-05-19 21:28:37 +0200

    api related updated

--- lib/kodi/tools/DllHelper.h
+++ lib/kodi/tools/DllHelper.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2005-2020 Team Kodi
+ *  Copyright (C) 2005-2018 Team Kodi
  *  This file is part of Kodi - https://kodi.tv
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
@@ -8,21 +8,42 @@
 
 #pragma once
 
+#ifdef __cplusplus
+
 #include <string>
+
+#include <dlfcn.h>
 #include <kodi/AddonBase.h>
 #include <kodi/Filesystem.h>
-#include <dlfcn.h>
 
-#define REGISTER_DLL_SYMBOL(functionPtr) \
-  CDllHelper::RegisterSymbol(functionPtr, #functionPtr)
-
-/// @brief Class to help with load of shared library functions
+//==============================================================================
+/// @ingroup cpp_kodi_tools_CDllHelper
+/// @brief Macro to translate the given pointer value name of functions to
+/// requested function name.
 ///
+/// @note This should always be used and does the work of
+/// @ref kodi::tools::CDllHelper::RegisterSymbol().
+///
+#define REGISTER_DLL_SYMBOL(functionPtr) \
+  kodi::tools::CDllHelper::RegisterSymbol(functionPtr, #functionPtr)
+//------------------------------------------------------------------------------
+
+namespace kodi
+{
+namespace tools
+{
+
+//==============================================================================
+/// @defgroup cpp_kodi_tools_CDllHelper class CDllHelper
+/// @ingroup cpp_kodi_tools
+/// @brief **Class to help with load of shared library functions**\n
 /// You can add them as parent to your class and to help with load of shared
 /// library functions.
 ///
-/// @note To use on Windows must you also include [dlfcn-win32](https://github.com/dlfcn-win32/dlfcn-win32) on your addon!\n\n
-/// Furthermore, this allows the use of Android where the required library is copied to an EXE useable folder.
+/// @note To use on Windows must you also include [dlfcn-win32](https://github.com/dlfcn-win32/dlfcn-win32)
+/// on your addon!\n\n
+/// Furthermore, this allows the use of Android where the required library is
+/// copied to an EXE useable folder.
 ///
 ///
 /// ----------------------------------------------------------------------------
@@ -34,22 +55,22 @@
 ///
 /// ...
 /// class CMyInstance : public kodi::addon::CInstanceAudioDecoder,
-///                     private CDllHelper
+///                     private kodi::tools::CDllHelper
 /// {
 /// public:
-///   CMyInstance(KODI_HANDLE instance);
+///   CMyInstance(KODI_HANDLE instance, const std::string& kodiVersion);
 ///   bool Start();
 ///
 ///   ...
 ///
-///   /* The pointers for on shared library exported functions */
+///   // The pointers for on shared library exported functions
 ///   int (*Init)();
 ///   void (*Cleanup)();
 ///   int (*GetLength)();
 /// };
 ///
-/// CMyInstance::CMyInstance(KODI_HANDLE instance)
-///   : CInstanceAudioDecoder(instance)
+/// CMyInstance::CMyInstance(KODI_HANDLE instance, const std::string& kodiVersion)
+///   : CInstanceAudioDecoder(instance, kodiVersion)
 /// {
 /// }
 ///
@@ -67,20 +88,34 @@
 /// ...
 /// ~~~~~~~~~~~~~
 ///
+///@{
 class CDllHelper
 {
 public:
+  //============================================================================
+  /// @ingroup cpp_kodi_tools_CDllHelper
+  /// @brief Class constructor.
+  ///
   CDllHelper() = default;
+  //----------------------------------------------------------------------------
+
+  //============================================================================
+  /// @ingroup cpp_kodi_tools_CDllHelper
+  /// @brief Class destructor.
+  ///
   virtual ~CDllHelper()
   {
     if (m_dll)
       dlclose(m_dll);
   }
+  //----------------------------------------------------------------------------
 
-  /// @brief Function to load requested library
+  //============================================================================
+  /// @ingroup cpp_kodi_tools_CDllHelper
+  /// @brief Function to load requested library.
   ///
-  /// @param[in] path         The path with filename of shared library to load
-  /// @return                 true if load was successful done
+  /// @param[in] path The path with filename of shared library to load
+  /// @return true if load was successful done
   ///
   bool LoadDll(std::string path)
   {
@@ -88,19 +123,21 @@ public:
     if (kodi::vfs::FileExists(path))
     {
       // Check already defined for "xbmcaltbinaddons", if yes no copy necassary.
-      std::string xbmcaltbinaddons = kodi::vfs::TranslateSpecialProtocol("special://xbmcaltbinaddons/");
+      std::string xbmcaltbinaddons =
+          kodi::vfs::TranslateSpecialProtocol("special://xbmcaltbinaddons/");
       if (path.compare(0, xbmcaltbinaddons.length(), xbmcaltbinaddons) != 0)
       {
         bool doCopy = true;
         std::string dstfile = xbmcaltbinaddons + kodi::vfs::GetFileName(path);
 
-        STAT_STRUCTURE dstFileStat;
+        kodi::vfs::FileStatus dstFileStat;
         if (kodi::vfs::StatFile(dstfile, dstFileStat))
         {
-          STAT_STRUCTURE srcFileStat;
+          kodi::vfs::FileStatus srcFileStat;
           if (kodi::vfs::StatFile(path, srcFileStat))
           {
-            if (dstFileStat.size == srcFileStat.size && CDllHelper::timespec_get(&dstFileStat.modificationTime, TIME_UTC) > CDllHelper::timespec_get(&srcFileStat.modificationTime, TIME_UTC))
+            if (dstFileStat.GetSize() == srcFileStat.GetSize() &&
+                dstFileStat.GetModificationTime() > srcFileStat.GetModificationTime())
               doCopy = false;
           }
         }
@@ -110,7 +147,8 @@ public:
           kodi::Log(ADDON_LOG_DEBUG, "Caching '%s' to '%s'", path.c_str(), dstfile.c_str());
           if (!kodi::vfs::CopyFile(path, dstfile))
           {
-            kodi::Log(ADDON_LOG_ERROR, "Failed to cache '%s' to '%s'", path.c_str(), dstfile.c_str());
+            kodi::Log(ADDON_LOG_ERROR, "Failed to cache '%s' to '%s'", path.c_str(),
+                      dstfile.c_str());
             return false;
           }
         }
@@ -132,11 +170,21 @@ public:
     }
     return true;
   }
+  //----------------------------------------------------------------------------
 
-  /// @brief Function to register requested library symbol
+  //============================================================================
+  /// @ingroup cpp_kodi_tools_CDllHelper
+  /// @brief Function to register requested library symbol.
   ///
-  /// @note This function should not be used, use instead the macro
-  /// REGISTER_DLL_SYMBOL to register the symbol pointer.
+  /// @warning This function should not be used, use instead the macro
+  /// @ref REGISTER_DLL_SYMBOL to register the symbol pointer.
+  ///
+  ///
+  /// Use this always via Macro, e.g.:
+  /// ~~~~~~~~~~~~~{.cpp}
+  /// if (!REGISTER_DLL_SYMBOL(Init))
+  ///   return false;
+  /// ~~~~~~~~~~~~~
   ///
   template <typename T>
   bool RegisterSymbol(T& functionPtr, const char* strFunctionPtr)
@@ -149,15 +197,15 @@ public:
     }
     return true;
   }
+  //----------------------------------------------------------------------------
 
 private:
-#if defined(TARGET_ANDROID)
-  // timespec_get is broken on Android
-  int timespec_get(timespec* ts, int base)
-  {
-    return (base == TIME_UTC && clock_gettime(CLOCK_REALTIME, ts) != -1) ? base : 0;
-  }
-#endif
-
   void* m_dll = nullptr;
 };
+///@}
+//------------------------------------------------------------------------------
+
+} /* namespace tools */
+} /* namespace kodi */
+
+#endif /* __cplusplus */
